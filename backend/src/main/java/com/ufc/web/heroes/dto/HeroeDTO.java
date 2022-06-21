ad2d3eb9cd04adb533f376a336962facb0b9619d
Renan Lima <86834473+RenanLima7@users.noreply.github.com>
2022-06-20 21:13:13 -0300

    Update HeroeDTO.java

--- backend/src/main/java/com/ufc/web/heroes/dto/HeroeDTO.java
+++ backend/src/main/java/com/ufc/web/heroes/dto/HeroeDTO.java
@@ -10,5 +10,22 @@ import lombok.*;
 public class HeroeDTO {
 	@NotBlank
 	@NotNull	
-	private String name; 
+	private String name;
+	
+	public HeroeDTO(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public HeroeDTO() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}	
 }
